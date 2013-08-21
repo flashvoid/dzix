65dbf589d28d99d9e44c7cb336eb43579782f3ab
Stas A. Kraev <stas.kraev@digitaldialogue.com.au>
2013-08-21 06:36:58 +0000

    Reinit in case of authentification timeout

--- libraries/dzix.rb
+++ libraries/dzix.rb
@@ -6,6 +6,16 @@ module Dzix
           Chef::Log.error("Missing gem 'zabbixapi'. Use the default aws recipe to install it first.")
         end
 
-        @@dzix ||= ZabbixApi.new(:url => new_resource.zabbix_url, :user => new_resource.zabbix_user, :password => new_resource.zabbix_password, :debug => new_resource.zabbix_debug)
+        begin
+          @@dzix.query(:method=>"apiinfo.version", :params => { } )
+        rescue
+          @@dzix = init
+        end
+
+        @@dzix ||= init
+      end
+
+      def init
+         @@init = ZabbixApi.new(:url => new_resource.zabbix_url, :user => new_resource.zabbix_user, :password => new_resource.zabbix_password, :debug => new_resource.zabbix_debug)
       end
 end
